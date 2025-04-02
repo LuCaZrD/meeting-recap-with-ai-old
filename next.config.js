a7177cb60b97dab10b74c03232d226d49f7e52ec
Quang Khai <tranquangkhaia6@gmail.com>
2025-04-02 10:43:15 +0700

    fix: cập nhật cấu hình Next.js và thêm middleware

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  compress: true,
   distDir: 'out',
   images: {
     unoptimized: true,
@@ -13,7 +14,34 @@ const nextConfig = {
   eslint: {
     // Bỏ qua lỗi ESLint trong quá trình build
     ignoreDuringBuilds: true,
-  }
+  },
+  webpack: (config, { isServer }) => {
+    // Tối ưu kích thước bundle
+    config.optimization = {
+      ...config.optimization,
+      splitChunks: {
+        chunks: 'all',
+        minSize: 20000,
+        maxSize: 24400000, // 24MB
+        minChunks: 1,
+        maxAsyncRequests: 30,
+        maxInitialRequests: 30,
+        cacheGroups: {
+          defaultVendors: {
+            test: /[\\/]node_modules[\\/]/,
+            priority: -10,
+            reuseExistingChunk: true,
+          },
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true,
+          },
+        },
+      },
+    }
+    return config
+  },
 };
 
 module.exports = nextConfig;
